d84ff262ee604a7c80f9122e7116110abf48b7b6
Abdul Hamid <45659176+abdulhamid24@users.noreply.github.com>
2018-12-09 22:02:35 +0700

    Add files via upload

---
+++ Menghitung Jarak Tempuh.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+int s,v,t;
+printf("                                                PROOGRAM MENGHITUNG JARAK TEMPUH\n");
+printf("                                                BY ABDUL HAMID NIM 311810167\n");
+printf("                                           INSTITUT PELITA BANGSA. TEKNIK INFORMATIKA\n\n");
+printf("Masukkan kecepatan (v) : ");scanf("%i",&v);
+printf("Masukkan waktu dalam sekon (t) : ");scanf("%i",&t);
+s=v*t;
+printf("S = %i * %i = %i Meter",v,t,s);
+getch();
+}
+
